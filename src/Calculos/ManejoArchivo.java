bf00d82dccfd0c3838bd91b0fb1a2facd27ec172
Ivan Arturo <ivandiaz94ing@gmail.com>
2024-11-20 21:48:02 -0500

    manejo de archivo terminado fin del proyecto

--- src/Calculos/ManejoArchivo.java
+++ src/Calculos/ManejoArchivo.java
@@ -1,4 +1,25 @@
 package Calculos;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import modelos.Serie;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 public class ManejoArchivo {
+
+    public void copiarArchivo (List<Serie> lista){
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try {
+            FileWriter archivo = new FileWriter("peliculas.json");
+            archivo.write(gson.toJson(lista));
+            archivo.close();
+            System.out.println("Archivo JSON creado exitosamente.");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -1,27 +1,54 @@
 package Principal;
 
 import Calculos.ConsultaAPI;
+import Calculos.ManejoArchivo;
 import modelos.Serie;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println("*******   Bienvenidos a MovieSync   *******");
-        int valor;
+        int valor=0;
+        String band;
         ConsultaAPI peticion = new ConsultaAPI();
+        ManejoArchivo archivo = new ManejoArchivo();
 
         Scanner lectura = new Scanner(System.in);
-        System.out.println("Seleccione una pelicula del 1 al 6");
-        try {
-        valor = lectura.nextInt();
-        Serie serie = peticion.getFilm(valor);
-        System.out.println(serie);
-        }catch (Exception e){
-            e.getMessage();
-        }
+        List<Serie> series = new ArrayList<>();
+
+        System.out.println("*******   Bienvenidos a MovieSync   *******");
+
+        do {
+            System.out.println("Seleccione una pelicula del 1 al 6");
+
+
+            try {
+                valor = lectura.nextInt();
+                Serie serie = peticion.getFilm(valor);
+                series.add(serie);
+                System.out.println(serie);
+
+            }catch (Exception e){
+                e.getMessage();
+            }
+
+            System.out.println();
+            System.out.println("**** Presione ´s´ para salir ****");
+            band = lectura.next();
+
+            if (band.equals("s")){
+                archivo.copiarArchivo(series);
+                break;
+            }
+
+
+
+        }while (valor!= 9);
+
 
     }
 }
